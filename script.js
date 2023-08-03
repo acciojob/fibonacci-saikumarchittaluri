fcc40ca2bd4677548332cfc2ea5bed7819bd8f1d
saikumarchittaluri <127985962+saikumarchittaluri@users.noreply.github.com>
2023-08-03 17:24:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function fibonacci(num) {
 // your code here
+	const memo = new Array(51).fill(-1); // Initialize an array to store computed Fibonacci numbers
+
+  function fibHelper(n) {
+    if (n <= 1) {
+      return n;
+    }
+
+    if (memo[n] !== -1) {
+      return memo[n];
+    }
+
+    memo[n] = fibHelper(n - 1) + fibHelper(n - 2);
+    return memo[n];
+  }
+
+  return fibHelper(num);
 }
 
 module.exports = fibonacci;
